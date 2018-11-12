a557536f31082f69060376b9363e81643ea6fa85
Karol Swieca <swieca.karol@gmail.com>
2018-11-12 21:52:33 +0100

    redux and rootReducer

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+import authReducer from './auth'
+import projectReducer from './project'
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  project: projectReducer
+})
+
+export default rootReducer
