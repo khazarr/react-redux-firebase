7075d506f787c4db5db5bae11c5f1e6cdc5ec460
Karol Swieca <swieca.karol@gmail.com>
2018-11-12 19:55:09 +0100

    sign up & sign in components

---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class SignIn extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange (e) {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+  handleSubmit (e) {
+    e.preventDefault()
+    console.log(this.state)
+  }
+  render () {
+    return (
+      <div className='container'>
+        <form onSubmit={this.handleSubmit} className='white'>
+          <h5 className='gray-text text-darken-3'>Sign In</h5>
+          <div className='input-field'>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' onChange={this.handleChange} />
+          </div>
+          <div className='input-field'>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' onChange={this.handleChange} />
+          </div>
+          <div className='input-field'>
+            <button className='btn lighten-1 z-depth-0'>Login</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignIn
