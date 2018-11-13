d5ec3904004dfc8e90fe001657554d3028c3c063
Karol Swieca <swieca.karol@gmail.com>
2018-11-13 20:11:48 +0100

    firestore live sync for projects

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import authReducer from './auth'
 import projectReducer from './project'
 import { combineReducers } from 'redux'
+import { firestoreReducer } from 'redux-firestore'
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  project: projectReducer
+  project: projectReducer,
+  firestore: firestoreReducer
 })
 
 export default rootReducer
