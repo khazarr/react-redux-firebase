ee5d8edc534102f191bc6ab2cb6102a986435284
Karol Swieca <swieca.karol@gmail.com>
2018-11-14 20:38:55 +0100

    firebase: log in

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { signIn } from '../../store/actions/authActions'
 
 class SignIn extends Component {
   constructor (props) {
@@ -18,8 +20,10 @@ class SignIn extends Component {
   handleSubmit (e) {
     e.preventDefault()
     console.log(this.state)
+    this.props.signIn(this.state)
   }
   render () {
+    const {authError} = this.props
     return (
       <div className='container'>
         <form onSubmit={this.handleSubmit} className='white'>
@@ -34,6 +38,9 @@ class SignIn extends Component {
           </div>
           <div className='input-field'>
             <button className='btn pink lighten-1 z-depth-0'>Login</button>
+            <div className="red-text center">
+              {authError ? <p>{authError}</p> : null}
+            </div>
           </div>
         </form>
       </div>
@@ -41,4 +48,16 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (creds) => dispatch(signIn(creds))
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    authError: state.auth.authError
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,7 +1,24 @@
-const initState = {}
+const initState = {
+  authError: null
+}
 
 const authReducer = (state = initState, action) => {
-  return state
+  switch (action.type) {
+    case 'LOGIN_ERROR':
+      console.log('login fail')
+      return {
+        ...state,
+        authError: 'Login failed'
+      }
+    case 'LOGIN_SUCCESS':
+      console.log('login sukces')
+      return {
+        ...state,
+        authError: null
+      }
+    default:
+      return state
+  }
 }
 
 export default authReducer
